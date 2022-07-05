55375ff2393effca5f5ed0fd900035e675333510
Sai Sathvik <91463407+Sathvik8500@users.noreply.github.com>
2022-07-05 15:41:41 +0530

    Passenger.java

---
+++ Passenger.java
@@ -0,0 +1,58 @@
+public class Passenger {
+	private String Name;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String theName) {
+		Name = theName;
+	}
+	private Integer Age;
+	public Integer getAge() {
+		return Age;
+	}
+	public void setAge(Integer theAge) {
+		Age = theAge;
+	}
+	private String Address;
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String theAddress) {
+		Address = theAddress;
+	}
+	private Payment payment;
+	public Payment getPayment() {
+		return payment;
+	}
+	public void setPayment(Payment thePayment) {
+		payment = thePayment;
+	}
+
+	private Ticket ticket;
+	public Ticket getTicket() {
+		return ticket;
+	}
+	public void setTicket(Ticket theTicket) {
+		ticket = theTicket;
+	}
+	private Train train;
+	public Train getTrain() {
+		return train;
+	}
+	public void setTrain(Train theTrain) {
+		train = theTrain;
+	}
+	public void SearchTrain() {
+	}
+	public void ViewSchedule() {
+	}
+	public void ReserveTicket() {
+	}
+	public void PurchaseTicket() {
+	}
+	public void MakePayment() {
+	}
+}
+
+
+
