e6447f2cfaa18e54310cbac62c91234471f3b376
Sai Sathvik <91463407+Sathvik8500@users.noreply.github.com>
2022-07-05 15:40:39 +0530

    Administrator.java

---
+++ Administrator.java
@@ -0,0 +1,31 @@
+public class Administrator {
+	private String Name;
+	public String getName() {
+		return Name;
+	}
+	public void setName(String theName) {
+		Name = theName;
+	}
+	private String Id;
+	public String getId() {
+		return Id;
+	}
+	public void setId(String theId) {
+		Id = theId;
+	}
+	private RailwayDB railwaydb;
+	public RailwayDB getRailwaydb() {
+		return railwaydb;
+	}
+	public void setRailwaydb(RailwayDB theRailwaydb) {
+		railwaydb = theRailwaydb;
+	}
+	public void CancelTicket() {
+	}
+	public void ReserveTicket() {
+	}
+	public void UpdateTrainDetails() {
+	}
+	public void CheckDB() {
+	}
+}
